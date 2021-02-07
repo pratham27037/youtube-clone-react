cc3405a663363ea85373957ba035bacb97b1c5ee
Pratham choudhary <prathamchoudhary27037@gmail.com>
2021-02-07 22:35:00 +0530

    almost final

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -24,12 +24,60 @@ function SearchPage() {
            <hr />
 
             <VideoRow 
-                views="136K Views"
+                views="136K"
                 subs="890K"
                 description="In this FREE LIVE training, Qazi & Sonny will show you how to build a YouTube Clone with REACT JS 🚀."
                 timestamp="29 july 2020"
                 channel = "Clever programmer"
-                title="Let's Build a YouTube Clone with REACT JS for Beginners"
+                title="🔴 Let's Build a YouTube Clone with REACT JS for Beginners"
+                image="http://i3.ytimg.com/vi/NT299zIk2JY/maxresdefault.jpg"
+            />
+            <VideoRow 
+                views="1.9M"
+                subs="890K"
+                description="If you want to enroll in an EPIC Python course where you can have exercises and projects all under one account & for FREE... Click this link: https://cleverprogrammer.teachable.co...​
+
+The tool I'm using to write my Python code online... https://repl.it​.
+"
+                timestamp="21 Dec 2019"
+                channel = "Clever programmer"
+                title="Python Tutorial for Beginners - Full Course in 11 Hours [2020]"
+                image="http://i3.ytimg.com/vi/4F2m91eKmts/maxresdefault.jpg"
+            />
+            <VideoRow 
+                views="250K"
+                subs="890K"
+                description="In this FREE LIVE training, Qazi & Sonny will show you how to build a YouTube Clone with REACT JS 🚀."
+                timestamp="8 july 2020"
+                channel = "Clever programmer"
+                title="🔴 How to Build a Instagram Clone with REACT JS for Beginners (in 3 Hours!)"
+                image="http://i3.ytimg.com/vi/f7T48W0cwXM/maxresdefault.jpg"
+            />
+            <VideoRow 
+                views="114K"
+                subs="890K"
+                description="If you want to enroll in an EPIC Beginner Python course where you can have exercises and projects all under one account & for FREE... Click this link: https://cleverprogrammer.teachable.co..."
+                timestamp="1 month ago"
+                channel = "Clever programmer"
+                title="Python Artificial Intelligence Tutorial - AI Full Course for Beginners in 9 Hours [2021]"
+                image="http://i3.ytimg.com/vi/XIrOM9oP3pA/maxresdefault.jpg"
+            />
+            <VideoRow 
+                views="136K"
+                subs="890K"
+                description="In this FREE LIVE training, Qazi & Sonny will show you how to build a YouTube Clone with REACT JS 🚀."
+                timestamp="29 july 2020"
+                channel = "Clever programmer"
+                title="🔴 Let's Build a YouTube Clone with REACT JS for Beginners"
+                image="http://i3.ytimg.com/vi/NT299zIk2JY/maxresdefault.jpg"
+            />
+            <VideoRow 
+                views="136K"
+                subs="890K"
+                description="In this FREE LIVE training, Qazi & Sonny will show you how to build a YouTube Clone with REACT JS 🚀."
+                timestamp="29 july 2020"
+                channel = "Clever programmer"
+                title="🔴 Let's Build a YouTube Clone with REACT JS for Beginners"
                 image="http://i3.ytimg.com/vi/NT299zIk2JY/maxresdefault.jpg"
             />
 
--- src/VideoRow.js
+++ src/VideoRow.js
@@ -4,7 +4,14 @@ import "./VideoRow.css"
 function VideoRow(props) {
     return (
         <div className="videoRow">
-            <h2>its a video row</h2>
+            <img src={props.image} alt=""></img>
+            <div className="videoRow__text">
+                <h3>{props.title}</h3>
+                <p className="videoRow__headline">
+                    {props.channel} . <span className="videoRow__subs"><span className="videoRow__subsNumber">{props.subs}</span> Subscribers</span> {props.views} views . {props.timestamp}
+                </p>
+                <p className="videoRow__description">{props.description}</p>
+            </div>
         </div>
     )
 }
